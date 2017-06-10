5d007078ebbe192200517eae2dd505631b229643
Royce <royce236@gmail.com>
2017-06-11 02:20:24 +0800

    addition of code to seek final

---
+++ seekFinal/seek.py
@@ -0,0 +1,38 @@
+import flask
+from flask import render_template, request, json, redirect, session
+#from flask.ext.mysql import MySQL
+from flaskext.mysql import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
+
+mysql = MySQL()
+seek = flask.Flask(__name__)
+seek.secret_key = 'a02304jire0942jitn092jf039ejinsifgj20j9ei0934runifjldkn-sdkjn'
+
+# MySQL configurations
+seek.config['MYSQL_DATABASE_USER'] = 'royce'
+seek.config['MYSQL_DATABASE_PASSWORD'] = '261523'
+seek.config['MYSQL_DATABASE_DB'] = 'seekdb'
+seek.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(seek)
+
+@seek.route('/')
+def main():
+    return render_template('')
+
+@seek.route('/login')
+def login():
+    return render_template('')
+
+
+
+@seek.route('/register')
+def register():
+    return render_template('')
+
+@seek.route('/createAccount',methods=['POST'])
+def createAccount():
+    if request.method=='POST':
+        # request.form['username']
+        pass
+    else:
+        request.method(401)
