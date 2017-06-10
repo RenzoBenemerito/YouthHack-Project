b565ce3300e59874f30636f94975a8225f87e691
miruhiii <ej.depaz03@gmail.com>
2017-06-11 06:33:13 +0800

    "updated for flask"

--- seekFinal/static/js/script.js
+++ seekFinal/static/js/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
     $("li>.button.special").click(function(event){
         event.preventDefault();
-        $("li>.button.special").fadeOut();
-        //$("li>.button.special").remove();
+        $("li>.button.special").fadeOut(function(){
+            $("li>.button.special").remove();
+            $("li>.button.special.spec").addClass('spec1');
+        });
+        
     });
 });
