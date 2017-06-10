e32f8a8b5cc5533f22a8551113018a3783163616
Royce <royce236@gmail.com>
2017-06-10 22:42:59 +0800

    registered royce

--- seek/loginApp/admin.py
+++ seek/loginApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . models import user_account, user_speaker, user_organization
 
-# Register your models here.
+admin.site.register(user_account)
+admin.site.register(user_speaker)
+admin.site.register(user_organization)
