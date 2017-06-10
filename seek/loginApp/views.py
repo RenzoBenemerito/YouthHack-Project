21916b5fc628b4d06501a6f7c80ddd6948433239
Royce <royce236@gmail.com>
2017-06-11 02:13:17 +0800

    views last edit

--- seek/loginApp/views.py
+++ seek/loginApp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Addition
 from loginApp.loginForm import LoginForm
+from loginApp.models import user_account,user_organization,user_speaker
 
 # Create your views here.
 def index(request):
@@ -14,10 +16,16 @@ def validateLogin(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            print("Form validation success")
-            print(form.cleaned_data['username'])
-            print(form.cleaned_data['password'])
-            return render(request,'index.html')
+            getusername=form.cleaned_data['username']
+            getpassword=form.cleaned_data['password']
+            nameval =user_account.objects.get
+            passval =user_account.objects.
+            print(nameval,passval)
+            if(nameval == getusername and passval == getpassword):
+                return render(request,'index.html')
+            else:
+                return HttpResponse('Login Failed')
+            
     else:
         form = LoginForm()
     return render(request,'loginApp/login.html',{'form':form})
